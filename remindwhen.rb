13dbd6c46bf53878f0566cb66a8683d277daf796
Sterling Camden <sterling@camdensoftware.com>
2010-05-28 13:44:51 -0700

    Audible alarm if ~/.when/reminder.wav exists, option to make reminder quiet even if it does, suppress test wrapping and paging from when.

--- remindwhen.rb
+++ remindwhen.rb
@@ -4,21 +4,29 @@ require 'time'
 def check_reminder(event)
   now = Time.now
   beg = now - 180		# show for three minutes past due time
-  if /\b(\d{1,2}:\d{1,2}(?:pm)?)\b\s*(?:\+(\d+))?/i =~ event
+  if /\b(\d{1,2}:\d{1,2}(?:pm)?)\b(\s*\+(\d*)(q)?)?/i =~ event
     ewhen = Time.parse $1
-    if $2
+    if $3
       target = now + ($2.to_i * 60)
     else 
       target = now
     end
-    puts event.sub(/^today\s+\d{4}\s\w+\s+\d+/,'Reminder:') if ((ewhen >= beg) && (ewhen <= target))
+    sound = !$4		# no q option
+    qual = $2 || ''	# all qualifiers
+    if ((ewhen >= beg) && (ewhen <= target))
+      puts event.sub(/^today\s+\d{4}\s\w+\s+\d+/,'Reminder:').sub(qual,'')
+      if sound
+	wav = File.expand_path("~/.when/reminder.wav")
+	system "mplayer #{wav} >/dev/null 2>&1" if File.exists? wav
+      end
+    end
   end
 end
 
 # first, check the reminders calendar
-reminders = `/usr/local/bin/when --calendar=~/.when/reminders --past=0 --future=0`.split("\n")[2..-1]
+reminders = `/usr/local/bin/when --calendar=~/.when/reminders --nopaging --wrap=0 --past=0 --future=0`.split("\n")[2..-1]
 reminders.each {|event| check_reminder event } if reminders
 
 # now, the main calendar
-events = `/usr/local/bin/when --past=0 --future=0`.split("\n")[2..-1]
+events = `/usr/local/bin/when --nopaging --wrap=0 --past=0 --future=0`.split("\n")[2..-1]
 events.each {|event| check_reminder event } if events
