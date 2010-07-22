28a3a2cfbbd7b926c50a67835d1522a7258b4eea
Sterling Camden <sterling@camdensoftware.com>
2010-07-21 21:11:23 -0700

    Add icalview.rb

---
+++ icalview.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "icalendar"
+require "date"
+
+class DateTime
+  def myformat
+    self.new_offset(Time.now.utc_offset/(24*60*60).to_f).strftime("%a %b %d %Y, %H:%M")
+  end
+end
+
+cals = Icalendar.parse($<)
+cals.each do |cal|
+  cal.events.each do |event|
+    puts "Organizer: #{event.organizer}"
+    puts "Event:     #{event.summary}"
+    puts "Starts:    #{event.dtstart.myformat} local time"
+    puts "Ends:      #{event.dtend.myformat}"
+    puts "Contact:   #{event.contacts}"
+    puts "Description:\n#{event.description}"
+    puts ""
+  end
+end
